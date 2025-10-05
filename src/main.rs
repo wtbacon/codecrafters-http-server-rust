bda1a8859fa095f952cadc207546bd87f3a98a86
wtbacon <wtbaaaacon@gmail.com>
2025-10-05 19:05:56 +0900

    feat: respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -6,13 +7,14 @@ fn main() {
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    
+
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                _stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
