883140b6595ccfb0d3f7972c99e002e61f245f42
wtbacon <wtbaaaacon@gmail.com>
2025-11-03 14:56:54 +0900

    feat: add files endpoint

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fs, path};
 
 use crate::http::{
     request::Request,
@@ -39,4 +39,44 @@ pub fn user_agent_handler(req: &Request, _params: HashMap<String, String>) -> Re
         .insert("Content-Type".to_string(), "text/plain".to_string());
 
     Response::new(head, Some(user_agent))
+}
+
+pub fn files_handler(req: &Request, params: HashMap<String, String>) -> Response {
+    let args = std::env::args().collect::<Vec<String>>();
+
+    if args.len() != 3 && args[1] != "--directory" {
+        eprint!("Server must be started with --directory <dir>");
+        eprint!("{:?}", args);
+        return Response::new(Parts::new(StatusCode::NOT_FOUND, req.head.version), None);
+    }
+    let dir = &args[2];
+
+    let filename = params.get("filename").unwrap_or(&"".to_string()).clone();
+    if filename
+        .split(path::MAIN_SEPARATOR)
+        .any(|part| part == "..")
+    {
+        eprint!("Invalid file path");
+        return Response::new(Parts::new(StatusCode::NOT_FOUND, req.head.version), None);
+    }
+
+    let full_path = format!("{}/{}", dir, filename);
+    let file_contents = match fs::read_to_string(full_path) {
+        Ok(contents) => contents,
+        Err(_) => {
+            eprint!("Failed to read file");
+            return Response::new(Parts::new(StatusCode::NOT_FOUND, req.head.version), None);
+        }
+    };
+
+    let mut head = Parts::new(StatusCode::OK, req.head.version);
+
+    head.headers.insert(
+        "Content-Length".to_string(),
+        file_contents.len().to_string(),
+    );
+    head.headers
+        .insert("Content-Type".to_string(), "text/plain".to_string());
+
+    Response::new(head, Some(file_contents))
 }
--- src/net.rs
+++ src/net.rs
@@ -29,31 +29,6 @@ pub fn run_server(addr: &str) {
 }
 
 fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
-    // let mut buf_reader = BufReader::new(stream);
-    // let mut stream_string = Vec::new();
-    // loop {
-    //     let mut line = String::new();
-
-    //     if let Err(e) = buf_reader.read_line(&mut line) {
-    //         eprintln!("Failed to read from connection: {}", e);
-    //         let response = Response::new(
-    //             StatusCode::from_u16(500).unwrap(),
-    //             None,
-    //             Some("Internal Server Error".to_string()),
-    //         );
-    //         stream.write_all(response.to_http().as_bytes()).unwrap();
-    //         stream.flush().unwrap();
-
-    //         return Ok(());
-    //     }
-
-    //     let line = line.trim();
-    //     if line.is_empty() {
-    //         break;
-    //     }
-    //     stream_string.push(line.to_string());
-    // }
-
     let request = parse_request(&mut stream)?;
     println!("{:?}", request);
 
@@ -61,6 +36,7 @@ fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
     router.add_route("GET", "/", root_handler);
     router.add_route("GET", "/echo/:msg", echo_handler);
     router.add_route("GET", "/user-agent", user_agent_handler);
+    router.add_route("GET", "/files/:filename", crate::handlers::files_handler);
 
     let response = router.route(&request);
     stream.write_all(response.to_http().as_bytes()).unwrap();
