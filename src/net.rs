9ebd706246cbc0500bd380933cd5c1b2410f29a1
wtbacon <wtbaaaacon@gmail.com>
2025-11-06 10:15:21 +0900

    fix: chore

--- src/net.rs
+++ src/net.rs
@@ -1,7 +1,7 @@
-use crate::handlers::{echo_handler, root_handler, user_agent_handler};
+use crate::handlers;
 use crate::http::request::Request;
 use crate::route::Router;
-use std::io::{Error, Write};
+use std::io::{self, Write};
 use std::{
     net::{TcpListener, TcpStream},
     thread,
@@ -28,25 +28,25 @@ pub fn run_server(addr: &str) {
     }
 }
 
-fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
+fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
     let request = Request::parse_request(&mut stream)?;
     println!("{:?}", request);
 
     let mut router = Router::new();
-    router.add_route("GET", "/", root_handler);
-    router.add_route("GET", "/echo/:msg", echo_handler);
-    router.add_route("GET", "/user-agent", user_agent_handler);
-    router.add_route("GET", "/files/:filename", crate::handlers::files_handler);
-    router.add_route(
-        "POST",
-        "/files/:filename",
-        crate::handlers::post_file_handler,
-    );
+    router.add_route("GET", "/", handlers::root_handler);
+    router.add_route("GET", "/echo/:msg", handlers::echo_handler);
+    router.add_route("GET", "/user-agent", handlers::user_agent_handler);
+    router.add_route("GET", "/files/:filename", handlers::files_handler);
+    router.add_route("POST", "/files/:filename", handlers::post_file_handler);
 
     let response = router.route(&request);
     println!("{:?}", response);
 
-    stream.write_all(response.to_http_headers_only().as_bytes()).unwrap();
-    stream.write_all(&response.body.clone().unwrap_or_default())?;
-    Ok(())
+    stream
+        .write_all(response.to_http_headers_only().as_bytes())
+        .unwrap();
+    stream
+        .write_all(&response.body.clone().unwrap_or_default())
+        .unwrap();
+    stream.flush()
 }
